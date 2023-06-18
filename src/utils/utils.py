825d1c26b6a5a9aa0606e16d83720fe29c5481c6
Haowei Lee <haoweilee.tpe@gmail.com>
2023-06-18 14:11:41 +0200

    feat: add fill_nan_str func

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -17,3 +17,13 @@ def generate_records(df: pd.DataFrame, n:int, target_col: str, start_col: str, e
             rows.append(new_row)
     new_df = pd.concat(rows, axis=1).T
     return new_df.reset_index(drop=True)
+
+
+def fill_nan_str(df, col:str, to_fill: str):
+    # Copy the dataframe to avoid modifying the original one
+    df_filled = df.copy()
+
+    # Replace NaN values
+    df_filled[col] = df_filled[col].fillna(to_fill)
+
+    return df_filled
