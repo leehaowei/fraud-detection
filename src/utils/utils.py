f1f116d4012dc070a8b733ee5dff221423043fc2
Haowei Lee <haoweilee.tpe@gmail.com>
2023-06-18 13:50:21 +0200

    feat: add generate_records func

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,3 +4,16 @@ import pandas as pd
 def inspector(df: pd.DataFrame, col, target):
     filter_ = df[col] == target
     return df[filter_]
+
+
+def generate_records(df: pd.DataFrame, n:int, target_col: str, start_col: str, end_col: str):
+    rows = []
+    for _, row in df.iterrows():
+        start_year = row[start_col] - n
+        end_year = row[end_col]
+        for year in range(start_year, end_year + 1):
+            new_row = row.copy()
+            new_row[target_col] = year
+            rows.append(new_row)
+    new_df = pd.concat(rows, axis=1).T
+    return new_df.reset_index(drop=True)
