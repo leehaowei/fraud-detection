152891f83bd4dd2d4ddf8248f8feb836046419e2
Haowei Lee <haoweilee.tpe@gmail.com>
2023-06-23 14:50:57 +0200

    feat: add plotting func in model package

--- src/model/model_tuning.py
+++ src/model/model_tuning.py
@@ -1,4 +1,6 @@
+import numpy as np
 import yaml
+from matplotlib import pyplot as plt
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
 from .model_pipeline import ModelPipeline
@@ -53,3 +55,48 @@ class ModelTuning:
 
         with open(file_path, "w") as file:
             yaml.dump(model_params_dict, file)
+
+    @staticmethod
+    def plot_comparison(
+        models_eval_dict, export_svg=False, filename="model_comparison.svg"
+    ):
+        """
+        Plot comparison of different models
+
+        :param models_eval_dict: a dictionary that contains model names as keys and corresponding evaluation metrics dataframes as values
+        :param export_svg: boolean, if True the plot will be saved to an SVG file
+        :param filename: string, name of the file where the plot will be saved (only if export_svg is True)
+        :return: the matplotlib figure and axes objects
+        """
+        metrics = ["Precision", "Recall", "F1 Score", "AUPRC", "AUCROC", "Accuracy"]
+        model_names = list(models_eval_dict.keys())
+        bar_width = 0.2
+        r = np.arange(len(metrics))  # the label locations
+
+        fig, ax = plt.subplots(figsize=(10, 7))
+
+        # Define color scheme
+        colors = ["#1f77b4", "#17becf", "#aec7e8"]  # shades of blue
+
+        # Create bars for each model
+        for i, model in enumerate(model_names):
+            model_scores = [
+                models_eval_dict[model].loc["KFold", metric] for metric in metrics
+            ]
+            r_model = [x + bar_width * i for x in r]
+            ax.barh(
+                r_model, model_scores, height=bar_width, color=colors[i], label=model
+            )
+
+        ax.set_xlabel("Score")
+        ax.set_xlim([0, 1])
+        ax.set_ylabel("Metric")
+        ax.set_yticks([r + bar_width for r in range(len(metrics))])
+        ax.set_yticklabels(metrics)
+        ax.legend()
+
+        # Export the plot to an SVG file if required
+        if export_svg:
+            plt.savefig(filename, format="svg")
+
+        return fig, ax
