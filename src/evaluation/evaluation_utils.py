4e2377331de5ddc06db16bbc8337195af9a092b0
Haowei Lee <haoweilee.tpe@gmail.com>
2023-07-11 11:18:54 +0200

    feat: add apply_prediction in evaluation

--- src/evaluation/evaluation_utils.py
+++ src/evaluation/evaluation_utils.py
@@ -26,3 +26,13 @@ def onset_deviation(y_true, y_pred):
         onset_pred = len(y_pred)
 
     return np.abs(onset_true - onset_pred)
+
+
+def apply_prediction(group, model):
+    # Ensure the data is in the correct shape for prediction
+    X = group.copy()
+    X = X.drop(columns=["motive", "gvkey", "year"])
+
+    # Predict and add results to original data
+    group["prediction"] = model.predict(X)
+    return group[["motive", "gvkey", "year", "prediction"]]
