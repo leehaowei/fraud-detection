959a1f1f05379714c400688698b8cd57f7e2d075
Haowei Lee <haoweilee.tpe@gmail.com>
2023-07-11 09:42:31 +0200

    feat: add funcs in utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -29,3 +30,32 @@ def fill_nan_str(df, col: str, to_fill: str):
     df_filled[col] = df_filled[col].fillna(to_fill)
 
     return df_filled
+
+
+def validate_comparable(df, comparable_dict: dict):
+    for target_gvkey in comparable_dict:
+        filter_1 = df["gvkey"] == target_gvkey
+        filter_2 = df["gvkey"] == comparable_dict[target_gvkey]["comparable"]
+        arr = df[filter_1]["year"].values == df[filter_2]["year"].values
+        print(target_gvkey)
+        print(np.any(arr == False))
+
+
+def keep_only_bv_records(df, data_dict):
+    concatenated_df = pd.DataFrame(columns=df.columns)
+
+    for key, info in data_dict.items():
+        filter_1 = (df["gvkey"] == key) | (df["gvkey"] == info["comparable"])
+
+        current_temp_df = df[filter_1]
+        bv_year = current_temp_df["year"].unique()[0]
+        # print(f"bv_year: {bv_year}")
+        filter_2 = df["year"] <= bv_year
+
+        filters = filter_1 & filter_2
+
+        df_to_add = df[filters]
+
+        concatenated_df = pd.concat([concatenated_df, df_to_add])
+
+    return concatenated_df
