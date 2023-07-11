5324ba675b61cd818f51d7b6022cb99bb27dd6b4
Haowei Lee <haoweilee.tpe@gmail.com>
2023-07-11 10:25:01 +0200

    feat: update evaluation funcs

--- src/evaluation/evaluation_utils.py
+++ src/evaluation/evaluation_utils.py
@@ -2,29 +2,23 @@ import numpy as np
 from sklearn.metrics import f1_score
 
 
-def combined_score(y_true, y_pred):
-    onset_dev = onset_deviation_normalized(y_true, y_pred)
+def combined_score(y_true, y_pred, alpha=0.5):
+    onset_dev = onset_deviation(y_true, y_pred)
     f1 = f1_score(y_true, y_pred)
+    score = alpha * (1 - onset_dev / len(y_true)) + (1 - alpha) * f1
 
-    return (onset_dev + f1) / 2
+    return score
 
 
-def onset_deviation_normalized(y_true, y_pred):
-    onsets_true = np.where(np.diff(y_true) == 1)[0] + 1
-    onsets_pred = np.where(np.diff(y_pred) == 1)[0] + 1
+def onset_deviation(y_true, y_pred):
+    try:
+        onset_true = np.where(y_true == 1)[0][0]
+    except:
+        onset_true = len(y_true)
 
-    if len(onsets_pred) == 0:
-        # If no fraud is predicted, return 0
-        return 0
-    else:
-        total_deviation = 0
-        for pred in onsets_pred:
-            # Find the closest true onset for each predicted onset
-            closest_true_onset = min(onsets_true, key=lambda x: abs(x - pred))
-            total_deviation += abs(pred - closest_true_onset) / len(y_true)
+    try:
+        onset_pred = np.where(y_pred == 1)[0][0]
+    except:
+        onset_pred = len(y_pred)
 
-        # Take the average of the deviations
-        avg_deviation = total_deviation / len(onsets_pred)
-
-        # Return the inverted and normalized score
-        return 1 - avg_deviation
+    return np.abs(onset_true - onset_pred)
