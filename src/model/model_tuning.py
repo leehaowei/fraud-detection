4cf5abd12739c13cfaf35d39cc096d8ef87f7d36
Haowei Lee <haoweilee.tpe@gmail.com>
2023-07-11 16:58:12 +0200

    feat: add func to plot auc-roc

--- src/model/model_tuning.py
+++ src/model/model_tuning.py
@@ -2,6 +2,7 @@ from joblib import dump
 import numpy as np
 import yaml
 from matplotlib import pyplot as plt
+from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
 from .model_pipeline import ModelPipeline
@@ -97,10 +98,59 @@ class ModelTuning:
         ax.set_ylabel("Metric")
         ax.set_yticks([r + bar_width for r in range(len(metrics))])
         ax.set_yticklabels(metrics)
-        ax.legend()
+
+        # Set the location of the legend to the bottom left of the plot
+        ax.legend(loc="upper right", bbox_to_anchor=(0.0, -0.10))
 
         # Export the plot to an SVG file if required
         if export_svg:
-            plt.savefig(filename, format="svg")
+            plt.savefig(filename, format="svg", bbox_inches="tight")
 
         return fig, ax
+
+    @staticmethod
+    def plot_roc_curves(X, y, models, title="Receiver Operating Characteristic"):
+        """
+        Plots ROC curves for models
+
+        :param X: feature dataset
+        :param y: target variable
+        :param models: a dictionary containing model names as keys and trained model instances as values
+        :param title: title of the plot
+        :return: the matplotlib figure and axes objects
+        """
+        plt.figure(figsize=(10, 8))
+        colors = ["#1f77b4", "#17becf", "#aec7e8"]  # shades of blue
+        lw = 2
+
+        for i, (model_name, model) in enumerate(models.items()):
+            probs = model.predict_proba(X)
+            preds = probs[:, 1]
+            fpr, tpr, threshold = roc_curve(y, preds)
+            roc_auc = auc(fpr, tpr)
+
+            plt.plot(
+                fpr,
+                tpr,
+                color=colors[i],
+                lw=lw,
+                label="%s ROC curve (area = %0.2f)" % (model_name, roc_auc),
+            )
+
+        # Include the line for a random classifier in the legend by giving it a label
+        plt.plot(
+            [0, 1],
+            [0, 1],
+            color="navy",
+            lw=lw,
+            linestyle="--",
+            label="Random Classifier (area = 0.50)",
+        )
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
+        plt.title(title)
+        plt.legend(loc="lower right")
+
+        return plt.figure, plt.gca()
